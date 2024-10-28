f796365b8c90f16298decf8d4533206e57cdd4f8
ALPHISTUBE <rsalphi@gmail.com>
2024-10-28 16:44:16 +0600

    project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,36 +1,27 @@
-// components/Projects.jsx
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import styled from 'styled-components';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { color } from 'chart.js/helpers';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
 const projects = [
-  { name: 'FPS', type: 'Unity', img: '/assets/unity/1.jpg' },
-  { name: 'Nature', type: 'Unity', img: '/assets/unity/2.jpg' },
-  { name: 'Shader', type: 'Unity', img: '/assets/unity/3.jpg' },
-  { name: 'Mini Game', type: 'Unity', img: '/assets/unity/4.jpg' },
+  { name: 'FPS', type: 'Game Development', img: '/assets/unity/1.jpg' },
+  { name: 'Nature', type: 'Game Development', img: '/assets/unity/2.jpg' },
+  { name: 'Shader', type: 'Game Development', img: '/assets/unity/3.jpg' },
+  { name: 'Mini Game', type: 'Game Development', img: '/assets/unity/4.jpg' },
   { name: 'React Website', type: 'Website', img: '/assets/website.png' },
-  // Add more projects
 ];
 
 const frameworks = [
-  { name: 'Unity', totalProjects: 1, totalYears: 2, logo: '/assets/unity.png' },
-  { name: 'React', totalProjects: 1, totalYears: 1, logo: '/assets/react.svg' },
-  { name: 'AI', totalProjects: 0, totalYears: 0, logo: '/assets/AI.png' },
-  { name: 'Blender', totalProjects: 0, totalYears: 0, logo: '/assets/blender.png' },
-  { name: 'Unreal', totalProjects: 0, totalYears: 0, logo: '/assets/unreal.png' },
-  { name: '.Net', totalProjects: 0, totalYears: 0, logo: '/assets/dotnet.png' },
-  { name: 'Photoshop', totalProjects: 0, totalYears: 0, logo: '/assets/ps.png' },
-  { name: 'Affer Effect', totalProjects: 0, totalYears: 0, logo: '/assets/ae.png' },
-  { name: 'Flutter', totalProjects: 0, totalYears: 0, logo: '/assets/flutter.png' },
-  { name: 'Android Studio', totalProjects: 0, totalYears: 0, logo: '/assets/androidS.png' },
+  { name: 'Game Development', totalProjects: 4, totalYears: 2, logo: ['/assets/unity.png', '/assets/unreal.png', '/assets/cpluse.png', '/assets/cSharp.png'] },
+  { name: 'React', totalProjects: 1, totalYears: 1, logo: ['/assets/react.svg'] },
 ];
 
 const Projects = () => {
-  const [selectedFramework, setSelectedFramework] = useState(null);
+  const [selectedFramework, setSelectedFramework] = useState(frameworks[0].name);
+  const [selectedProject, setSelectedProject] = useState(projects[0]);
 
   const filteredProjects = selectedFramework
     ? projects.filter(project => project.type === selectedFramework)
@@ -42,177 +33,296 @@ const Projects = () => {
 
   const settings = {
     infinite: true,
-    speed: 500,
+    speed: 500, // Increased for smoother transition
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 2000, // Autoplay speed increased
     centerMode: true,
     centerPadding: '0px',
+    arrows: false,
     responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-        }
-      }
-    ]
+      { breakpoint: 768, settings: { slidesToShow: 1 } },
+    ],
   };
 
+  const slider = React.useRef(null);
+
   return (
-    <ProjectsSection>
-      <h2 style={{ color: '#ffa200' }}>PROJECTS</h2>
-      <FrameworkDetails>
-        {frameworks.map((framework, index) => (
-          <FrameworkCard key={index} onClick={() => setSelectedFramework(framework.name)}>
-            <img id='filter' src={framework.logo} alt={framework.name} />
-          </FrameworkCard>
-        ))}
-      </FrameworkDetails>
-      <div style={{ display: 'flex', width: '100%', gap: '20px' }}>
-        {selectedFramework && (
-          <FrameworkDetailsCard
-          className='framework-details'
-          style={{ flex: 1 }}
-          initial={{ opacity: 0, scale: 0.8 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
-        >
-          <h3>{selectedFrameworkDetails.name}</h3>
-          <p>Total Projects: {selectedFrameworkDetails.totalProjects}</p>
-          <p>Total Years: {selectedFrameworkDetails.totalYears}</p>
-          </FrameworkDetailsCard>
-        )}
-        <ProjectList style={{ flex: 2 }}>
-          <Slider {...settings}>
-            {filteredProjects.map((project, index) => (
-              <motion.div 
-                key={index} 
-                initial={{ opacity: 0, y: 50 }} 
-                whileInView={{ opacity: 1, y: 0 }} 
-                transition={{ duration: 0.5 }}
+    <Container>
+      <Content>
+        <LeftSide>
+        <Header>
+            {frameworks.map((framework) => (
+              <FrameworkCard
+                key={framework.name}
+                isActive={framework.name === selectedFramework}
+                onClick={() => setSelectedFramework(framework.name)}
               >
-                <ProjectCard>
-                  <img src={project.img} alt={project.name} />
-                  <h3 style={{color: '#adb5bd'}}>{project.name}</h3>
-                </ProjectCard>
-              </motion.div>
+                <span>{framework.name}</span>
+              </FrameworkCard>
             ))}
-          </Slider>
-        </ProjectList>
-      </div>
-    </ProjectsSection>
+          </Header>
+          <FrameworkDetails>
+            <h1>{selectedFrameworkDetails?.name || 'Select a Framework'}</h1>
+            <p>Total Projects: {selectedFrameworkDetails?.totalProjects || 0}</p>
+            <p>Total Years: {selectedFrameworkDetails?.totalYears || 0}</p>
+            <h3>Description</h3>
+            <p>Framework-related description goes here.</p>
+            <h3>Used</h3>
+            <div>
+              {selectedFrameworkDetails?.logo.map((logo, index) => (
+                <img key={index} src={logo} alt={`${selectedFrameworkDetails.name} logo ${index + 1}`} />
+              ))}
+            </div>
+          </FrameworkDetails>
+          <SliderWrapper>
+            <StyledArrow className="prev" onClick={() => slider.current.slickPrev()}>
+              <FaChevronLeft />
+            </StyledArrow>
+            <Slider ref={slider} {...settings}>
+              {filteredProjects.map((project) => (
+                <motion.div 
+                  key={project.name} 
+                  initial={{ opacity: 0, y: 50 }} 
+                  animate={{ opacity: 1, y: 0 }} 
+                  transition={{ duration: 0.5 }}
+                  onClick={() => setSelectedProject(project)}
+                >
+                  <ProjectCard>
+                    <img src={project.img} alt={project.name} />
+                    <h3>{project.name}</h3>
+                  </ProjectCard>
+                </motion.div>
+              ))}
+            </Slider>
+            <StyledArrow className="next" onClick={() => slider.current.slickNext()}>
+              <FaChevronRight />
+            </StyledArrow>
+          </SliderWrapper>
+        </LeftSide>
+        <RightSide>
+          <SelectedProject key={selectedProject.name}>
+            <img src={selectedProject?.img} alt={selectedProject?.name} />
+            <h1>{selectedFramework.name}</h1>
+          </SelectedProject>
+        </RightSide>
+      </Content>
+    </Container>
   );
 };
 
 export default Projects;
 
-const ProjectsSection = styled.section`
-  padding: 50px;
-  background-color: #282c34;
+// Styled Components
+const Container = styled.div`
+  padding: 20px;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  h2 {
-    font-size: 2rem;
-    font-weight: 400;
-    color: #ffa200;
-    margin-bottom: 20px;
-  }
-  @media (max-width: 768px) {
-    padding: 20px;
-  }
+  background-color: #282c34;
+  font-family: 'Roboto', sans-serif; /* Popular font for portfolios */
 `;
 
-const FrameworkDetails = styled.div`
+const Header = styled.div`
   display: flex;
-  flex-wrap: wrap;
-  gap: 20px;
-  margin-bottom: 20px;
-  justify-content: center;
+  justify-content: flex-start; /* Changed from center to flex-start */
+  gap: 5px; /* Reduced gap */
+  margin-left: 10px; /* Reduced margin */
+  margin-top: 10px; /* Reduced margin */
   @media (max-width: 768px) {
-    width: auto;
+    flex-direction: column;
+    align-items: flex-start; /* Changed from left to flex-start */
+    margin-left: 20px;
   }
 `;
 
 const FrameworkCard = styled.div`
-  background-color: #444;
-  border-radius: 8px;
-  padding: 20px;
   cursor: pointer;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  transition: transform 0.3s, box-shadow 0.3s;
+  text-align: center;
+  color: ${({ isActive }) => (isActive ? '#fff' : '#adb5bd')};
+  background-color: ${({ isActive }) => (isActive ? '#ffa200' : '#282c34')};
+  transition: color 0.3s, transform 0.3s, box-shadow 0.3s;
+  padding: 3px 10px 7px 10px;
+  border-radius: 8px;
+
   &:hover {
     transform: translateY(-5px);
-    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
-  }
-  h3 {
-    margin: 0;
+    box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.2);
   }
 
-  #filter {
+  img {
     width: 50px;
-    height: auto;
+    height: 50px;
+    object-fit: contain;
+  }
+  span {
+    display: block;
+    margin-top: 5px;
+    font-size: 1.7rem;
   }
 `;
 
-const ProjectList = styled.div`
+const Content = styled.div`
   display: flex;
-  flex-direction: column;
-  overflow-y: hidden;
   gap: 20px;
-  max-height: 400px;
   width: 100%;
   @media (max-width: 768px) {
-    max-height: none;
+    flex-direction: column;
+  }
+`;
+
+const LeftSide = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 50%;
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+const RightSide = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 50%;
+  @media (max-width: 768px) {
+    width: 100%;
+    margin-top: 20px;
+  }
+`;
+
+const FrameworkDetails = styled.div`
+  padding: 20px;
+  border-radius: 8px;
+  color: white;
+  text-align: left;
+
+  h1 {
+    color: #ffa200;
+    font-size: 1.7rem;
+    margin-bottom: 10px;
+  }
+  p {
+    margin-top: 20px;
+    color: #adb5bd;
+    margin: 5px 0;
+    font-size: 1.5rem;
+  }
+  h3 {
+    color: #ffa200; /* Different color for h3 */
+    margin: 15px 0 0 0;
+    font-size: 1.7rem;
+  }
+
+  div {
+    margin-top: 10px;
+    display: flex; /* Added flex display */
+    gap: 10px; /* Optional: Add some gap between logos */
+    padding-right: 20px; /* Optional: Add some padding to the left */
+    img {
+      width: 50px;
+      height: 50px;
+      object-fit: contain;
+    }
   }
 `;
 
 const ProjectCard = styled.div`
-  background-color: #444;
   border-radius: 8px;
-  width: 300px;
-  padding: 20px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  transition: transform 0.3s, box-shadow 0.3s;
+  padding: 10px;
+  cursor: pointer;
+  text-align: center;
+  margin: 0 10px;
+  transition: transform 0.3s ease, box-shadow 0.3s ease;
+
   &:hover {
-    transform: translateY(-5px);
-    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+    transform: scale(1.05);
+    box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.2);
   }
+
   img {
     width: 100%;
-    height: auto;
     border-radius: 8px;
+    height: auto;
+    object-fit: contain;
   }
   h3 {
-    margin-top: 10px;
-  }
-  @media (max-width: 768px) {
-    width: 75%;
+    color: #ffa200;
+    margin-top: 5px;
+    text-align: left;
   }
 `;
 
-const FrameworkDetailsCard = styled(motion.div)`
-  flex: 1;
-  background-color: #333;
-  border-radius: 12px;
-  padding: 30px;
-  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.1);
-  transition: transform 0.3s, box-shadow 0.3s;
-  &:hover {
-    transform: translateY(-10px);
-    box-shadow: 0 12px 24px rgba(0, 0, 0, 0.2);
+const SelectedProject = styled.div`
+  border-radius: 8px;
+  color: white;
+  text-align: center;
+
+  h1 {
+    color: #ffa200;
+    font-size: 1.5rem;
+    margin-bottom: 10px;
   }
-  h3 {
-    margin-top: 0;
-    font-size: 1.5em;
-    color: #fff;
+
+  img {
+    width: 98%;
+    border-radius: 8px;
+    height: auto;
+    object-fit: contain;
+    margin-top: 20px;
   }
-  p {
-    font-size: 1.2em;
-    color: #fff;
-  }
+`;
+
+const SliderWrapper = styled.div`
+  position: relative; // Ensure the arrows are positioned relative to this wrapper
+  width: 95%;
+  padding-top: 20px;
+  border: 5px solid #1c2025; // Deeper color
+  background-color: #1c2025; // Deeper color
+  margin-left: 15px;
+  padding-right: 15px;
+  margin-bottom: 15px;
   @media (max-width: 768px) {
-    display: none;
+    width: 100%;
+    margin-left: 0;
+    padding-right: 0;
+  }
+`;
+
+const StyledArrow = styled.div`
+  position: absolute; // Position absolutely within the SliderWrapper
+  top: 50%; // Center vertically
+  transform: translateY(-50%); // Adjust to align properly
+  cursor: pointer;
+  z-index: 100; // Ensure it appears above other elements
+  transition: transform 0.3s ease, color 0.3s ease; // Smooth transition for hover effects
+
+  &.prev {
+    left: 10px; // Position on the left
+  }
+
+  &.next {
+    right: 10px; // Position on the right
+  }
+
+  svg {
+    color: #fff; // Color for the arrow icons
+    font-size: 24px; // Size for the arrow icons
+    padding: 10px; // Padding around the arrow icons
+    border-radius: 50%; // Make the arrow icons circular
+    position: center; // Center the arrow icons
+    border: 2px solid #fff; // Border color for the arrow icons
+    background-color: #ffa200; // Background color for the arrow icons
+  }
+
+  &:hover {
+    svg {
+      background-color: #fff; // Background color for the arrow icons
+      color: #ffa200; // Change color on hover (golden color)
+      transform: scale(1.2); // Slightly enlarge the icon
+    }
   }
 `;
